d18677e96129926d115273ada9255f1766cf1fca
Hari Karam Singh <harikaram@club15cc.com>
2012-02-01 12:35:11 +0000

    Initial Commit

---
+++ MusicalLib/MLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MLAppDelegate.h
+//  MusicalLib
+//
+//  Created by Hari Karam Singh on 01/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MusicalLibTests/MusicalLibTests.h
@@ -0,0 +1,13 @@
+//
+//  MusicalLibTests.h
+//  MusicalLibTests
+//
+//  Created by Hari Karam Singh on 01/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MusicalLibTests : SenTestCase
+
+@end
